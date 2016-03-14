68fe66c81bae6a7cde3a2601f8892d6bb642faea
Costin Galan <cgalan@cloudbasesolutions.com>
2016-03-14 13:12:48 +0200

    Release IP for a faster boot process

--- UnattendResources/Logon.ps1
+++ UnattendResources/Logon.ps1
@@ -65,6 +65,15 @@ function Run-Defragment {
     }
 }
 
+function Release-IP {
+    $HOST.UI.RawUI.WindowTitle = "Releasing IP..."
+    ipconfig.exe /release
+    if ($LASTEXITCODE)
+    {
+        throw "IPconfig release failed"
+    }
+}
+
 try
 {
     Import-Module "$resourcesDir\ini.psm1"
@@ -126,6 +135,8 @@ try
 
     Clean-UpdateResources
     
+    Release-IP
+
     $Host.UI.RawUI.WindowTitle = "Running Sysprep..."
     $unattendedXmlPath = "$programFilesDir\Cloudbase Solutions\Cloudbase-Init\conf\Unattend.xml"
     Set-PersistDrivers -Path $unattendedXmlPath -Persist:$persistDrivers
